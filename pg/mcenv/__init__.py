9c920346f3f6b7d5fe8fcb52dc6f41a5c738738a
Richard Hsu <shengwh1@uci.edu>
2020-03-08 22:02:20 -0700

    updated pg

--- pg/mcenv/__init__.py
+++ pg/mcenv/__init__.py
@@ -3,5 +3,5 @@ from gym.envs.registration import register
 register(
     id='MinecraftEnv-v0',
     entry_point='mcenv.minecraft_env:MinecraftEnv',
-	kwargs={'mission_file': 'flatQ2.xml'},
+	kwargs={'mission_file': 'pg_gym.xml'},
 )
